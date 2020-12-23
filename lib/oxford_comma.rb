ff5b6719e0a4dde8caafc4a93a9ccffa917f9d1e
Michael Penn <mwpenn94@gmail.com>
2020-12-23 02:04:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
   if array.size == 2
     array.join(' and ') 
-  elsif 
-  array[-1] = "and " + array[-1]
+  elsif array.size > 2
   array.join(', ')
+  array[-1] = "and " + array[-1]
+  else array.join
+  end
 end
